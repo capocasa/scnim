a99858a7846b7c5ccd055cfe3864fa950b68c5f9
Carlo Capocasa <carlo@capocasa.net>
2018-03-09 15:24:16 +0100

    ptrmath

---
+++ ptrmath.nim
@@ -0,0 +1,18 @@
+# Jehan @ http://forum.nim-lang.org/t/1188/1
+template `+`*[T](p: ptr T, off: int): ptr T =
+  cast[ptr type(p[])](cast[ByteAddress](p) +% off * sizeof(p[]))
+
+template `+=`*[T](p: ptr T, off: int) =
+  p = p + off
+
+template `-`*[T](p: ptr T, off: int): ptr T =
+  cast[ptr type(p[])](cast[ByteAddress](p) -% off * sizeof(p[]))
+
+template `-=`*[T](p: ptr T, off: int) =
+  p = p - off
+
+template `[]`*[T](p: ptr T, off: int): T =
+  (p + off)[]
+
+template `[]=`*[T](p: ptr T, off: int, val: T) =
+  (p + off)[] = val
